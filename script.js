d51862e577c587c6092b4f99ee1d968d910fb3a9
jdm-git <58112825+jdm-git@users.noreply.github.com>
2020-01-26 21:13:10 +0100

    styles

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ $.ajax({
     jsonpCallback: "callback",
     dataType: "jsonp",
     success: function(location) {
-        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+location.city+"&APPID=2eab94e68e577bc023a653cc6be04573",
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+location.city+"&lang=pl&APPID=2eab94e68e577bc023a653cc6be04573",
         function(data){
             console.log(data);
         
             var weatherIcon = "https://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
             var temperature = data.main.temp;
-            var weather = data.weather[0].main;
+            var weather = data.weather[0].description;
             console.log({weatherIcon, temperature, weather});
             document.getElementById("icon").src=weatherIcon;
             document.getElementById("temperature").innerHTML = Math.round(temperature-273)+" &#8451;";
             document.getElementById("weather").innerText = weather;
-            document.getElementById("city").innerText = location.city;
+            document.getElementById("city").innerText = data.name;
+        }
+        )
+        $.getJSON("https://api.openweathermap.org/data/2.5/forecast?q="+location.city+"&lang=pl&appid=2eab94e68e577bc023a653cc6be04573",
+        function(forecast){
+            console.log(forecast);
         }
         )
     }
@@ -23,18 +28,18 @@ $.ajax({
   function checkWeather(){
 
     let city = document.getElementById("cityName").value;
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=2eab94e68e577bc023a653cc6be04573",
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+city+"&lang=pl&APPID=2eab94e68e577bc023a653cc6be04573",
     function(data){
         console.log(data);
     
         var weatherIcon = "https://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
         var temperature = data.main.temp;
-        var weather = data.weather[0].main;
+        var weather = data.weather[0].description;
         
         document.getElementById("icon").src=weatherIcon;
         document.getElementById("temperature").innerHTML = Math.round(temperature-273)+" &#8451;";
         document.getElementById("weather").innerText = weather;
-        document.getElementById("city").innerText = city;
+        document.getElementById("city").innerText = data.name;
     })
   }
   function drawWeather(){
